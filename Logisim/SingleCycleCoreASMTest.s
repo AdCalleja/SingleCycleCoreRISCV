80c0d12f15a6638fd2535821a2dc4d63286be89a
ACV99 <adri.calleja.1999@gmail.com>
2021-09-25 16:54:13 +0200

    First commit

---
+++ Logisim/SingleCycleCoreASMTest.s
@@ -0,0 +1,27 @@
+#Logisim Tests
+
+
+
+
+#Code section - Machine code - Text
+#Defined by Program Counter(pc)
+#Grows UPWARDS (?)
+.text
+main:
+    lw a0, 0 (a0)
+    lw a1, 8 (a1)
+    add a2, a1, a0
+    sw a2, 8 (t0)
+    beq a2, a1, EQUAL #6 - 5
+    sub a3, a2, a1 #6 - 5
+    sub a2, a2, a3 #6 - 1 = 5: a2 == a1
+    beq a2, a1, EQUAL
+    nop
+    nop
+    nop
+    nop
+EQUAL:
+    and a3, a3, a2 #1 AND 5 = 1
+    or a4, a3, a2 #1 OR 5 = 5
+    
+    
