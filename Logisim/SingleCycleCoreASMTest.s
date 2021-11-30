7ccdffcf783a12eb10f44f4c24ef444bc2e02a9a
adrian <adrian@pop-os.localdomain>
2021-11-30 16:49:44 +0100

    Test correct and some cleans.

--- Logisim/SingleCycleCoreASMTest.s
+++ Logisim/SingleCycleCoreASMTest.s
@@ -1,19 +1,21 @@
-#Logisim Test
+#Logisim Tests
+#[addr]
+#(value inside)
 
 .text
 main:
-    lw a0, 0 (a0)
-    lw a1, 8 (a1)
-    add a2, a1, a0
-    sw a2, 8 (t0)
-    beq a2, a1, EQUAL #6 - 5
-    sub a3, a2, a1 #6 - 5
-    sub a2, a2, a3 #6 - 1 = 5: a2 == a1
-    beq a2, a1, EQUAL
+    lw a0, 0 (a0)     # Load from Data[0+0](1) to x10(0)
+    lw a1, 4 (a1)     # Load from Data[0+4](2) to x11(0)
+    add a2, a1, a0    # Add 1 + 2 = 3 to x12
+    sw a2, 4 (t0)     # Save x12(3) to Data[0+4]
+    beq a2, a1, EQUAL # Don't brach x12(3) - x11(2) = 1
+    sub a3, a2, a1    # Sub x12(3) - x11(2) = x13(1)
+    sub a2, a2, a3    # Sub x12(3) - x13(1) = x12(2) a2 == a1
+    beq a2, a1, EQUAL # Branch x12(2) - x11(2) = 0
     nop
     nop
     nop
     nop
 EQUAL:
-    and a3, a3, a2 #1 AND 5 = 1
-    or a4, a3, a2 #1 OR 5 = 5
+    and a3, a3, a2    # x13(1) AND x12(2) = x13(0)
+    or a4, a3, a2     # x13(1) OR x12(2) = x14(1)
--- RTL/build/runAlhambra-ii.sh
+++ RTL/build/runAlhambra-ii.sh
@@ -8,7 +8,7 @@ while getopts 'D:' flag; do
 done
 echo 'Defines  = -D${defines}=1'
 #Listen to Verilator Warnings
-verilator -Wall -cc ./../src/SingleCycleCore.v --prefix SSC -I./../src/
+#verilator -Wall -cc ./../src/SingleCycleCore.v --prefix SSC -I./../src/
 #Synthesize
 yosys -p "read_verilog -I./../src/ -D${defines}=1 ./../src/SingleCycleCore.v; synth_ice40 -json hardware.json" -q #./../src/SingleCycleCore.v
 #Place and Route
